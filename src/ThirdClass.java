dac781310000980a71596dae825c682857dd7040
voksus <voksus@gmail.com>
2017-01-27 01:38:58 +0200

    My first commit

---
+++ src/ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by voksus on 26.01.2017.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
